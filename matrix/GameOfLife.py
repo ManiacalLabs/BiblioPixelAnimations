b810031975be34ce56ac0fa9d7378b0fbdd08e77
adammhaile <adammhaile@gmail.com>
2014-12-22 22:48:18 -0500

    adding initial examples

---
+++ matrix/GameOfLife.py
@@ -0,0 +1,192 @@
+import random
+import time
+import copy
+from collections import deque
+
+from bibliopixel.animation import BaseMatrixAnim
+import bibliopixel.colors as colors
+
+class Table:
+
+    def __init__(self, height, width, rand_max, table=None):
+        self.toroidal = True
+        self._rand_max = rand_max
+        if table:
+            self.table = table
+            self.height = len(table)
+            self.width = len(table[0])
+        else:
+            self.height = height
+            self.width = width
+            self.genNewTable()
+
+        self._oldStates = deque()
+        for i in range(3):
+            self._oldStates.append([])
+
+    def genNewTable(self):
+        self.table = []
+
+        random.seed(time.time())
+        for y in range(0,self.height):
+            self.table.append([])
+            for x in range(0,self.width):
+                rand = random.randint(0,self._rand_max)
+                if rand == 0:
+                    self.table[y].append(1)
+                else:
+                    self.table[y].append(0)
+
+    def liveNeighbours(self, y, x):
+        """Returns the number of live neighbours."""
+        count = 0
+        if y > 0:
+            if self.table[y-1][x]:
+                count = count + 1
+            if x > 0:
+                if self.table[y-1][x-1]:
+                    count = count + 1
+            if self.width > (x + 1):
+                if self.table[y-1][x+1]:
+                    count = count + 1
+
+        if x > 0:
+            if self.table[y][x-1]:
+                count = count + 1
+        if self.width > (x + 1):
+            if self.table[y][x+1]:
+                count = count + 1
+
+        if self.height > (y + 1):
+            if self.table[y+1][x]:
+                count = count + 1
+            if x > 0:
+                if self.table[y+1][x-1]:
+                    count = count + 1
+            if self.width > (x + 1):
+                if self.table[y+1][x+1]:
+                    count = count + 1
+
+        if self.toroidal:
+            if y == 0:
+                if self.table[self.height-1][x]:
+                    count = count + 1
+            if y == self.height-1:
+                if self.table[0][x]:
+                    count = count + 1
+            if x == 0:
+                if self.table[y][self.width-1]:
+                    count = count + 1
+            if x == self.width-1:
+                if self.table[y][0]:
+                    count = count + 1
+
+        return count
+
+    def turn(self):
+        """Turn"""
+        nt = copy.deepcopy(self.table)
+        #nt = list(self.table)
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                neighbours = self.liveNeighbours(y, x)
+                if self.table[y][x] == 0:
+                    if neighbours == 3:
+                        nt[y][x] = 1
+                else:
+                    if (neighbours < 2) or (neighbours > 3):
+                        nt[y][x] = 0
+
+        self._oldStates.append(self.table)
+        if len(self._oldStates) > 3:
+            self._oldStates.popleft()
+
+        self.table = nt
+
+    def checkStable(self):
+        for t in self._oldStates:
+            if self.table == t:
+                return True
+        return False
+
+class GameOfLife(BaseMatrixAnim):
+    def __init__(self, led, color = colors.Red, bg = colors.Off, toroidal = False):
+        super(GameOfLife, self).__init__(led)
+
+        self._color = color
+        self._bg = bg
+        self._table = Table(self._led.height, self._led.width, 1, None)
+        self._table.toroidal = toroidal
+
+        self._finishCount = 0
+
+    def stepTable(self):
+        x = 0
+        y = 0
+        for row in self._table.table:
+            for col in row:
+                if col == 0:
+                    self._led.set(x, y, self._bg)
+                else:
+                    self._led.set(x, y, self._color)
+                x = x + 1
+            y = y + 1
+            x = 0
+
+        self._table.turn()
+
+    def step(self, amt = 1):
+
+        self.stepTable()
+        if self._table.checkStable():
+            self._finishCount += 1
+            if self._finishCount > 10:
+                self._table.genNewTable()
+                self._finishCount = 0
+
+        self._step = 0
+
+class GameOfLifeRGB(BaseMatrixAnim):
+    def __init__(self, led, toroidal = True):
+        super(GameOfLifeRGB, self).__init__(led)
+
+        self._tableR = Table(self._led.height, self._led.width, 1, None)
+        self._tableR.toroidal = toroidal
+
+        self._tableG = Table(self._led.height, self._led.width, 1, None)
+        self._tableG.toroidal = toroidal
+
+        self._tableB = Table(self._led.height, self._led.width, 1, None)
+        self._tableB.toroidal = toroidal
+
+    def stepTables(self):
+        x = 0
+        y = 0
+        for row in range(self._tableR.height):
+            for col in range(self._tableR.width):
+                r = (self._tableR.table[row][col]) * 255
+                g = (self._tableG.table[row][col]) * 255
+                b = (self._tableB.table[row][col]) * 255
+
+                c = (r, g, b)
+                self._led.set(x, y, c)
+
+                x = x + 1
+            y = y + 1
+            x = 0
+
+        self._tableR.turn()
+        self._tableG.turn()
+        self._tableB.turn()
+
+    def doStableCheck(self, table):
+        if table.checkStable():
+            table.genNewTable()
+
+    def step(self, amt = 1):
+
+        self.stepTables()
+        self.doStableCheck(self._tableR)
+        self.doStableCheck(self._tableG)
+        self.doStableCheck(self._tableB)
+        self._step = 0
---
+++ matrix/TicTacToe.py
@@ -0,0 +1,207 @@
+#much of the code below borrowed from http://cwoebker.com/posts/tic-tac-toe
+import random
+from bibliopixel.animation import BaseMatrixAnim
+import bibliopixel.colors as colors
+
+class Tic(object):
+    winning_combos = (
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6])
+
+    winners = ('X-win', 'Draw', 'O-win')
+
+    def __init__(self, squares=[]):
+        if len(squares) == 0:
+            self.clearBoard()
+        else:
+            self.squares = squares
+
+    def clearBoard(self):
+        self.squares = [None for i in range(9)];
+
+    def available_moves(self):
+        """what spots are left empty?"""
+        return [k for k, v in enumerate(self.squares) if v is None]
+
+    def available_combos(self, player):
+        """what combos are available?"""
+        return self.available_moves() + self.get_squares(player)
+
+    def complete(self):
+        """is the game over?"""
+        if None not in [v for v in self.squares]:
+            return True
+        if self.winner() != None:
+            return True
+        return False
+
+    def X_won(self):
+        return self.winner() == 'X'
+
+    def O_won(self):
+        return self.winner() == 'O'
+
+    def tied(self):
+        return self.complete() == True and self.winner() is None
+
+    def winner(self):
+        for player in ('X', 'O'):
+            positions = self.get_squares(player)
+            for combo in self.winning_combos:
+                win = True
+                for pos in combo:
+                    if pos not in positions:
+                        win = False
+                        break
+                if win:
+                    return player
+        return None
+
+    def winningCombo(self, player):
+        positions = self.get_squares(player)
+        winCombo = None
+        for combo in self.winning_combos:
+            win = True
+            for pos in combo:
+                if pos not in positions:
+                    win = False
+                    break
+            if win:
+                winCombo = combo
+                break
+
+        return winCombo
+
+    def get_squares(self, player = None):
+        """squares that belong to a player"""
+        if player:
+            return [k for k, v in enumerate(self.squares) if v == player]
+        else:
+            return self.squares
+
+    def get_enemy(self, player):
+        if player == 'X':
+            return 'O'
+        return 'X'
+
+    def make_move(self, position, player):
+        """place on square on the board"""
+        self.squares[position] = player
+
+    def alphabeta(self, node, player, alpha, beta):
+        if node.complete():
+            result = 0
+            if node.X_won():
+                result = 1
+            elif node.tied():
+                result = 0
+            elif node.O_won():
+                result = -1
+            if player == 'O':
+                result = result * -1
+            return result
+
+        for move in node.available_moves():
+            node.make_move(move, player)
+            val = self.alphabeta(node, self.get_enemy(player), alpha, beta)
+            node.make_move(move, None)
+            if player == 'O':
+                if val > alpha:
+                    alpha = val
+                if alpha >= beta:
+                    return beta
+            else: 
+                if val < beta:
+                    beta = val
+                if beta <= alpha:
+                    return alpha
+        if player == 'O':
+            return alpha
+        else:
+            return beta
+
+class TicTacToe(BaseMatrixAnim):
+    def __init__(self, led):
+        super(TicTacToe, self).__init__(led)
+
+        self._board = Tic()
+
+        self._player = 'O'
+        self._nextPlayer = 'O'
+
+        self._showWinCount = 0
+
+    def determine(self, board, player):
+        a = -2
+        choices = []
+        if len(board.available_moves()) == 9:
+            return random.randint(0,8)
+        for move in board.available_moves():
+            board.make_move(move, player)
+            val = board.alphabeta(board, self._board.get_enemy(player), -2, 2)
+            board.make_move(move, None)
+            if val > a:
+                a = val
+                choices = [move]
+            elif val == a:
+                choices.append(move)
+        return random.choice(choices)
+
+    def drawMove(self, index, player):
+        x = index % 3
+        y = index / 3
+        cx = 3 + (x * 8)
+        cy = 3 + (y * 8)
+
+        if player == 'X':
+            self._led.drawLine(cx - 2, cy - 2, cx + 2, cy + 2, colors.Green)
+            self._led.drawLine(cx - 2, cy + 2, cx + 2, cy - 2, colors.Green)
+        elif player == 'O':
+            self._led.drawCircle(cx, cy, 2, colors.Blue)
+
+    def step(self, amt = 1):
+        winner = None
+        winCombo = None
+        complete = self._board.complete()
+
+        if complete:
+            winner = self._board.winner()
+            if winner:
+                winCombo = self._board.winningCombo(winner)
+
+        self._led.all_off()
+
+        #right now just assuming 24x24, don't feel like doing the math for dynamic
+        self._led.drawLine(7, 0, 7, 22, colors.Red)
+        self._led.drawLine(15, 0, 15, 22, colors.Red)
+        self._led.drawLine(0, 7, 22, 7, colors.Red)
+        self._led.drawLine(0, 15, 22, 15, colors.Red)
+
+        for i in range(9):
+            self.drawMove(i, self._board.get_squares()[i])
+
+        if winCombo:
+            i = winCombo[0]
+            x1 = ((i % 3) * 8) + 3
+            y1 = ((i / 3) * 8) + 3
+            i = winCombo[2]
+            x2 = ((i % 3) * 8) + 3
+            y2 = ((i / 3) * 8) + 3
+            self._led.drawLine(x1, y1, x2, y2, colors.White)
+
+        self._step = 0
+
+        if complete and self._showWinCount > 2:
+            self._showWinCount = 0
+            self._board.clearBoard()
+            self._player = self._nextPlayer
+            self._nextPlayer = self._board.get_enemy(self._nextPlayer)
+            self.animComplete = True
+        elif complete:
+            self._showWinCount += 1
+
+        if not complete:
+            new_move = self.determine(self._board, self._player)
+            self._board.make_move(new_move, self._player)
+            self._player = self._board.get_enemy(self._player)
