660e8032e334fa30eedf95d70eef4da55e5895b9
adammhaile <adammhaile@gmail.com>
2015-01-11 08:45:13 -0500

    added hexclock

---
+++ strip/hexclock.py
@@ -0,0 +1,20 @@
+# Author: Adam Haile
+# Inspired by http://whatcolourisit.scn9a.org/
+
+import time
+from bibliopixel.animation import *
+import bibliopixel.colors as colors
+import bibliopixel.log as log
+class HEXClock(BaseStripAnim):
+
+    def __init__(self, led):
+        super(HEXClock, self).__init__(led, 0, -1)
+
+    def step(self, amt = 1):
+        t = time.localtime()
+        hex = "#{0:0>2}{1:0>2}{2:0>2}".format(t.tm_hour, t.tm_min, t.tm_sec)
+        log.info(hex)
+        c = colors.hex2rgb(hex)
+        self._led.fill(c)
+
+        self._step = 0
