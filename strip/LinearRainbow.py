2eec32d91fec3d9779f779835f09ce15f91cd786
Jon Carlos <jon@screenmedia.co.uk>
2015-04-28 21:13:26 +0100

    Fixed incorrect property names and added and individual pixel and all pixels setting. Also the previous version was not updating the first pixel properly

--- strip/LinearRainbow.py
+++ strip/LinearRainbow.py
@@ -2,24 +2,32 @@ from bibliopixel.animation import *
 
 class LinearRainbow(BaseStripAnim):
 
-    def __init__(self, led, maxLed, sleep = 0.01):
+    def __init__(self, led, max_led, sleep=0.01, individual_pixel=False):
         super(LinearRainbow, self).__init__(led, 0, -1)
         self._step = 0
         self._current = 0
         self._minLed = 0
-        self._maxLed = maxLed
+        self._maxLed = max_led
         self._delay = sleep
+        self._individualPixel = individual_pixel
 
-    def step(self, amt = 1):
-        self._led.fill(colors.wheel_color(self._step), 0, self._current)
+    def step(self, amt=1):
+        if self._individualPixel:
+            # This setting will change the colour of each pixel on each cycle
+            self._led.fill(colors.wheel_color(self._step), self._current, self._current)
+
+        else:
+            # This setting will change the colour of all pixels on each cycle
+            self._led.fill(colors.wheel_color(self._step), 0, self._current)
 
         if self._step == len(colors._wheel)-1:
             self._step = 0
+        else:
+            self._step += 1
 
         if self._current == self._maxLed:
             self._current = self._minLed
-
-        self._step += 1
-        self._current += 1
+        else:
+            self._current += 1
 
         time.sleep(self._delay)
