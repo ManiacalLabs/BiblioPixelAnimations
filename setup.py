8c58e9e5cd5380672149d1200ad2f6820704314c
Adam <Adam.Haile@emc.com>
2015-08-19 13:57:11 -0400

    forcing to use pip only

--- setup.py
+++ setup.py
@@ -7,22 +7,27 @@ _ver = "9.9.9b"
 if len(head_data) > 0:
     _ver = head_data[0]["object"]["sha"]
 
-print __file__
-
-setup(
-    name='BiblioPixelAnimations',
-    version=_ver,
-    description='BiblioPixelAnimations is an animation repository for animation classes that work with BiblioPixel: http://github.com/maniacallabs/BiblioPixel',
-    author='Adam Haile',
-    author_email='adam@maniacallabs.com',
-    url='http://github.com/maniacallabs/BiblioPixelAnimations/',
-    license='MIT',
-    packages=['BiblioPixelAnimations', 'BiblioPixelAnimations.matrix', 'BiblioPixelAnimations.strip'],
-    classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-    ],
-)
+if not 'pip' in __file__:
+    print """
+    This installer MUST be run from pip!
+    Please install using the following command:
+    pip install git+https://github.com/ManiacalLabs/BiblioPixelAnimations.git --upgrade
+    """
+else:
+    setup(
+        name='BiblioPixelAnimations',
+        version=_ver,
+        description='BiblioPixelAnimations is an animation repository for animation classes that work with BiblioPixel: http://github.com/maniacallabs/BiblioPixel',
+        author='Adam Haile',
+        author_email='adam@maniacallabs.com',
+        url='http://github.com/maniacallabs/BiblioPixelAnimations/',
+        license='MIT',
+        packages=['BiblioPixelAnimations', 'BiblioPixelAnimations.matrix', 'BiblioPixelAnimations.strip'],
+        classifiers = [
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+        ],
+    )
