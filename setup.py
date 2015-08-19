d9a5563ea3a6cb4cbeeec3491ec83d113f333c34
Adam <Adam.Haile@emc.com>
2015-08-19 13:09:05 -0400

    Reworking layout to enable setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+import bibliopixel
+
+setup(
+    name='BiblioPixel',
+    version="1.4",
+    description='BiblioPixelAnimations is an animation repository for animation classes that work with BiblioPixel: http://github.com/maniacallabs/BiblioPixel',
+    author='Adam Haile',
+    author_email='adam@maniacallabs.com',
+    url='http://github.com/maniacallabs/BiblioPixelAnimations/',
+    license='MIT',
+    packages=['BiblioPixelAnimations', 'BiblioPixelAnimations.matrix', 'BiblioPixelAnimations.strip'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
