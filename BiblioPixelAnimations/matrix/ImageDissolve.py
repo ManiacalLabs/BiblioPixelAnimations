7c93619d82201b2888167fe173a9cbb517693cde
adammhaile <adammhaile@gmail.com>
2015-09-19 20:25:30 -0400

    Added ImageDissolve and TallClock

---
+++ BiblioPixelAnimations/matrix/ImageDissolve.py
@@ -0,0 +1,64 @@
+from bibliopixel.animation import BaseMatrixAnim
+import bibliopixel.colors as colors
+from bibliopixel.image import loadImage, showImage
+from random import randint, shuffle
+
+class ImageDissolve(BaseMatrixAnim):
+
+    def __init__(self, led, imageFiles, pixelRate=10):
+        super(ImageDissolve, self).__init__(led)
+        if not isinstance(imageFiles, list) or len(imageFiles) == 0:
+            raise ValueError("imageFiles must be a list of file paths.")
+        self.pixelRate = pixelRate
+        self.imageFiles = imageFiles
+        self.imgIndex = 0
+        self.resetAndLoad()
+
+    def resetAndLoad(self):
+        self._led.setTexture(loadImage(self._led, imagePath=self.imageFiles[self.imgIndex]))
+        self.map = [(x,y) for x in range(self.width) for y in range(self.height)]
+        shuffle(self.map)
+
+    def step(self, amt):
+        for i in range(self.pixelRate):
+            x,y = self.map.pop()
+            self._led.set(x,y)
+            if len(self.map) == 0:
+                if len(self.imageFiles) == 1:
+                    self._led.all_off()
+                    self.animComplete = True
+                else:
+                    self.imgIndex += 1
+                    if self.imgIndex >= len(self.imageFiles):
+                        self.animComplete = True
+                        self.imgIndex = 0
+                self.resetAndLoad()
+                self.animComplete = True
+                break
+
+
+MANIFEST = [
+    {
+        "class": ImageDissolve,
+        "controller": "matrix",
+        "desc": "Dissolve betwen images",
+        "display": "Image Dissolve",
+        "id": "ImageDissolve",
+        "params": [{
+            "id": "imageFiles",
+            "label": "Files",
+            "type": "str_multi",
+            "default": [],
+            "help":"Image files to load.",
+            "replace": {"\\":"/"}
+        },{
+            "id": "pixelRate",
+            "label": "Pixel Rate",
+            "type": "int",
+            "default": 10,
+            "min": 1,
+            "help":"Number of pixels to add per frame.",
+        }],
+        "type": "animation"
+    }
+]
--- BiblioPixelAnimations/matrix/TallClock.py
+++ BiblioPixelAnimations/matrix/TallClock.py
@@ -8,14 +8,17 @@ class TallClock(BaseMatrixAnim):
         super(TallClock, self).__init__(led)
 
     def step(self, amt):
+        self._led.setTexture([[colors.hue_helper(y, self.height, self._step*2)]*self.width for y in range(self.height)])
         self._led.all_off()
         t = time.localtime()
         hrs = str(t.tm_hour).zfill(2)
         min = str(t.tm_min).zfill(2)
         sec = str(t.tm_sec).zfill(2)
-        self._led.drawText(hrs, x = 2, y = 2, color = colors.Red, size = 2)
-        self._led.drawText(min, x = 2, y = 18, color = colors.Red, size = 2)
-        self._led.drawText(sec, x = 2, y = 34, color = colors.Red, size = 2)
+        self._led.drawText(hrs, x = 2, y = 2, size = 2)
+        self._led.drawText(min, x = 2, y = 18, size = 2)
+        self._led.drawText(sec, x = 2, y = 34, size = 2)
+
+        self._step += amt
 
 
 MANIFEST = [
