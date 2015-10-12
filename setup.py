fcd0858decfa58de4437dce107e2a11fa7a0cb53
adammhaile <adammhaile@gmail.com>
2015-10-11 21:24:06 -0400

    added game folder to installer

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ else:
         author_email='adam@maniacallabs.com',
         url='http://github.com/maniacallabs/BiblioPixelAnimations/',
         license='MIT',
-        packages=['BiblioPixelAnimations', 'BiblioPixelAnimations.matrix', 'BiblioPixelAnimations.strip'],
+        packages=['BiblioPixelAnimations', 'BiblioPixelAnimations.matrix', 'BiblioPixelAnimations.strip', 'BiblioPixelAnimations.game'],
         classifiers = [
             'Development Status :: 5 - Production/Stable',
             'License :: OSI Approved :: MIT License',
