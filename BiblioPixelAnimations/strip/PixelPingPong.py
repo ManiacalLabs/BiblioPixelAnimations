2c9bd477fb37e878f270e1da035ee65a704cce67
Bonnie Barrilleaux <bonnie@Bonnies-MacBook-Air.local>
2016-02-28 20:48:45 -0800

    add feature to allow a fading trail of pixels in PixelPingPong pattern

--- BiblioPixelAnimations/strip/PixelPingPong.py
+++ BiblioPixelAnimations/strip/PixelPingPong.py
@@ -2,17 +2,18 @@
 # This animation runs 1 or many pixels from one end of a strip to the other.
 #
 # ### Usage ###
-# Alternates has 3 optional properties
+# Alternates has 4 optional properties
 #
 # * max_led - int the number of pixels you want used
 # * color - (int, int, int) the color you want the pixels to be
 # * additional_pixels - int the number of pixels you want to ping pong
+# * fade_delay - int the number of frames for the trailing pixels to fade to black
 #
 # In code:
 #
-# 	from PixelPingPong import PixelPingPong
-# 	...
-# 	anim = PixelPingPong(led, max_led=30, color=(0, 0, 255), additional_pixels=5)
+#   from PixelPingPong import PixelPingPong
+#   ...
+#   anim = PixelPingPong(led, max_led=30, color=(0, 0, 255), additional_pixels=5, fade_delay=2)
 #
 # Best run in the region of 5-200 FPS
 
@@ -21,7 +22,7 @@ from bibliopixel.animation import *
 
 class PixelPingPong(BaseStripAnim):
 
-    def __init__(self, led, max_led=None, color=(255, 255, 255), total_pixels=1):
+    def __init__(self, led, max_led=None, color=(255, 255, 255), total_pixels=1, fade_delay=1):
         super(PixelPingPong, self).__init__(led, 0, -1)
         self._current = 0
         self._minLed = 0
@@ -31,9 +32,14 @@ class PixelPingPong(BaseStripAnim):
         self._additionalPixels = total_pixels - 1
         self._positive = True
         self._color = color
+        self._fade_delay = fade_delay if fade_delay >= 1 else 1;
+        self._fade_increment = tuple(x/self._fade_delay for x in self._color)
 
     def step(self, amt=1):
-        self._led.fill((0, 0, 0), 0, self._maxLed)
+        # fade last frame's pixels
+        for i in xrange(0, self._maxLed + 1):
+            faded_color = tuple(x - self._fade_increment[i] if x > self._fade_increment[i] else 0 for (i, x) in enumerate(self._led.get(i)))
+            self._led.fill(faded_color, i, i)
 
         self._led.fill(
             self._color, self._current, self._current + self._additionalPixels)
