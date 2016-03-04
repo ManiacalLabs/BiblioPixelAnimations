4b58357c675ac4c3f752294047980ba2f084c6e6
adammhaile <adammhaile@gmail.com>
2016-03-04 18:17:28 -0500

    Added Mainframe Mainifest

--- BiblioPixelAnimations/matrix/Mainframe.py
+++ BiblioPixelAnimations/matrix/Mainframe.py
@@ -1,14 +1,12 @@
 from bibliopixel.animation import BaseMatrixAnim
 import bibliopixel.colors as colors
-from bibliopixel.util import genVector
 
-import math
 import os
 
 
 class Mainframe(BaseMatrixAnim):
 
-    def __init__(self, led, scroll = True, color = colors.Red, bgcolor = colors.Off):
+    def __init__(self, led, scroll=True, color=colors.Red, bgcolor=colors.Off):
         super(Mainframe, self).__init__(led)
         self.color = color
         self.bgcolor = bgcolor
@@ -26,7 +24,8 @@ class Mainframe(BaseMatrixAnim):
 
     def step(self, amt=8):
         if self.scroll:
-            new_bytes = [i for i in bytearray(os.urandom(self.rand_bytes_rows))]
+            new_bytes = [i for i in bytearray(
+                os.urandom(self.rand_bytes_rows))]
             for y in range(self.rand_bytes_rows):
                 self.bytes[y].pop(0)
                 self.bytes[y].append(new_bytes[y])
@@ -35,25 +34,48 @@ class Mainframe(BaseMatrixAnim):
 
         for y in range(self.height):
             for x in range(self.width):
-                self._led.set(self.width - x - 1, y, self.color if self.__getBit(x, y) else self.bgcolor)
+                self._led.set(self.width - x - 1, y,
+                              self.color if self.__getBit(x, y) else self.bgcolor)
 
 
-# MANIFEST = [
-#     {
-#         "class": Bloom,
-#         "controller": "matrix",
-#         "desc": "Rainbow blooming animation.",
-#         "display": "Bloom",
-#         "id": "Bloom",
-#         "params": [
-#             {
-#                 "default": True,
-#                 "help": "On for bloom in, off for bloom out.",
-#                 "id": "dir",
-#                 "label": "Direction",
-#                 "type": "bool"
-#             }
-#         ],
-#         "type": "animation"
-#     }
-# ]
+MANIFEST = [
+    {
+        "class": Mainframe,
+        "controller": "matrix",
+        "desc": "90's Computer Mainframe random blinking lights",
+        "display": "Mainframe",
+        "id": "Mainframe",
+        "params": [
+            {
+                "default": True,
+                "help": "Move and random",
+                "id": "scroll",
+                "label": "Scroll",
+                "type": "bool"
+            },
+            {
+                "default": [
+                    0,
+                    0,
+                    0
+                ],
+                "help": "",
+                "id": "bgcolor",
+                "label": "Background",
+                "type": "color"
+            },
+            {
+                "default": [
+                    255,
+                    0,
+                    0
+                ],
+                "help": "Random pixel color",
+                "id": "color",
+                "label": "Color",
+                "type": "color"
+            }
+        ],
+        "type": "animation"
+    }
+]
