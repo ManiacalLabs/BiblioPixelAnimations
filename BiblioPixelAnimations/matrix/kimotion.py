6b0bb3158330119fb89a547717d04ede3f11bd03
adammhaile <adammhaile@gmail.com>
2016-09-14 00:48:00 -0400

    recreating sandstorm

--- BiblioPixelAnimations/matrix/kimotion.py
+++ BiblioPixelAnimations/matrix/kimotion.py
@@ -11,6 +11,62 @@ WS_FRAME_WIDTH = 640
 WS_FRAME_HEIGHT = 480
 WS_FRAME_SIZE = WS_FRAME_WIDTH * WS_FRAME_HEIGHT
 
+def clamp(v, _min, _max):
+    return max(min(v, _max), _min)
+
+
+def lerp(n, low, high):
+    return clamp((n - low) / (high - low), 0.0, 1.0)
+
+
+MIN_Z = 440.0
+MAX_Z = 1100.0
+
+NEAR_Z = 760.0
+MID_Z = ((MAX_Z + NEAR_Z) / 2.0)
+FAR_Z = MAX_Z
+
+# MIN_Z = 0.0
+# MAX_Z = 65000.0
+#
+# NEAR_Z = 2000.0
+# MID_Z = ((MAX_Z + NEAR_Z) / 2.0)
+# FAR_Z = 60000.0
+
+near_color = np.array([255, 0, 0])  # np.array(colors.hex2rgb('#e56b00'))
+mid_color = np.array([0, 0, 255])  # np.array(colors.hex2rgb('#280072'))
+far_color = np.array([0, 255, 0])  # np.array(colors.hex2rgb('#02020c'))
+
+near_color = np.array(colors.hex2rgb('#e56b00'))
+mid_color = np.array(colors.hex2rgb('#280072'))
+far_color = np.array(colors.hex2rgb('#02020c'))
+# far_color = np.array([0, 0, 255])
+
+def z_color(z):
+    z = float(z)
+    alpha = 1.0
+
+    if z <= MID_Z:
+        ns = lerp(z, NEAR_Z, MID_Z)
+        color = (1.0 - ns) * near_color + ns * mid_color
+    else:  # z must be between MID_Z and FAR_Z
+        fs = lerp(z, MID_Z, FAR_Z)
+        color = (1.0 - fs) * mid_color + fs * far_color
+
+    alpha = 1.0 - lerp(z, MIN_Z, MAX_Z)
+
+    if z <= -MIN_Z:
+        alpha = 0.0
+
+    # gl_FragColor = vec4(color, alpha) * texture2D( texture, gl_PointCoord )
+
+    return (color * alpha).astype(np.uint8).tolist()
+
+
+def rebin(a, shape):
+    sh = shape[0],a.shape[0]//shape[0],shape[1],a.shape[1]//shape[1]
+    return a.reshape(sh).mean(-1).mean(1)
+
 
 def thread_lock():
     e = threading.Event()
@@ -30,7 +86,7 @@ class ws_thread(threading.Thread):
         self._stop = threading.Event()
         self._reading = thread_lock()
         self.dt = np.dtype(np.uint16)
-        self.dt = self.dt.newbyteorder('>')
+        self.dt = self.dt.newbyteorder('<')
         self._data = [np.zeros(WS_FRAME_SIZE, self.dt),
                       np.zeros(WS_FRAME_SIZE, self.dt)]
         self._buf = False
@@ -62,6 +118,7 @@ class Kimotion(BaseMatrixAnim):
 
     def __init__(self, led, server="localhost:1337", mirror=True, crop=True):
         super(Kimotion, self).__init__(led)
+        self.max_depth = 1200
         self.server = server
         self.mirror = mirror
         self.crop = crop
@@ -86,9 +143,7 @@ class Kimotion(BaseMatrixAnim):
             self.crop_box[1] = half
             self.crop_box[3] = self.resize_box[1] - half
 
-        self.lut = np.array(colors.hue_gradient(255, 0, self.max + 1)).astype(np.uint8)
-        print self.lut
-        print self.lut.shape
+        self.z_colors = np.array([z_color(z) for z in range(0, self.max_depth + 1)]).tolist()
         self._ws_thread = ws_thread(self.server)
         self._ws_thread.start()
 
@@ -97,23 +152,47 @@ class Kimotion(BaseMatrixAnim):
 
     def step(self, amt=1):
         d = self._ws_thread.get_frame()
-        d = self.lut[d]
         d = d.reshape(WS_FRAME_HEIGHT, WS_FRAME_WIDTH)
-
         if self.mirror:
             d = np.fliplr(d)
-        img = Image.fromarray(d)
-        img = img.resize(self.resize_box, Image.BILINEAR)
-        if self.crop:
-            img = img.crop(self.crop_box)
-            img.load()
 
+        d = rebin(d, (self.height, self.width)).astype(np.uint16)
         for y in range(self.height):
             for x in range(self.width):
-                p = img.getpixel((x, y))
-                if p == 0:
-                    c = colors.Black
-                else:
-                    c = colors.hue2rgb(p)
-                    # c = self.gradient[p]
+                c = self.z_colors[d[y][x]]
                 self._led.set(x, y, c)
+
+
+MANIFEST = [
+    {
+        "class": Kimotion,
+        "controller": "matrix",
+        "desc": "Pull Kinect data from Michael Clayton's Kimotion server",
+        "display": "Kimotion",
+        "id": "Kimotion",
+        "params": [
+            {
+                "default": "localhost:1337",
+                "help": "Kimotion server address (minus the ws://)",
+                "id": "server",
+                "label": "Server",
+                "type": "str"
+            },
+            {
+                "default": True,
+                "help": "Crop input video to display size.",
+                "id": "crop",
+                "label": "Crop",
+                "type": "bool"
+            },
+            {
+                "default": False,
+                "help": "Mirrors image along vertical. Useful for webcam video.",
+                "id": "mirror",
+                "label": "Mirror",
+                "type": "bool"
+            }
+        ],
+        "type": "animation"
+    }
+]
