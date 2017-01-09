5790684a2fa98788179d7d49259ae52d92271b00
Adam Haile <adammhaile@gmail.com>
2017-01-09 17:50:40 -0500

    Added Hyperspace

---
+++ BiblioPixelAnimations/circle/hyperspace.py
@@ -0,0 +1,167 @@
+from bibliopixel.animation import BaseCircleAnim
+import bibliopixel.colors as colors
+import random
+
+
+class Hyperspace(BaseCircleAnim):
+    def __init__(self, led, colors=[colors.Green], tail=4, growthRate=4, angleDiff=6):
+        super(Hyperspace, self).__init__(led)
+        if not isinstance(colors, list):
+            colors = [colors]
+        self._colors = colors
+        self._tail = tail
+        self._tails = [[] for x in range(360)]
+        self._growthRate = growthRate
+        self._angleDiff = angleDiff
+
+    def _drawTail(self, angle, ring, color):
+        for i in range(self._tail):
+            if ring - i >= 0 and ring - i <= self.lastRing:
+                level = 255 - ((255 / self._tail) * i)
+                self._led.set(ring - i, angle, colors.color_scale(color, level))
+
+    def step(self, amt=1):
+        self._led.all_off()
+
+        for i in range(self._growthRate):
+            newTail = random.randrange(0, 360, self._angleDiff)
+            cInt = random.randint(0, len(self._colors) - 1)
+            self._tails[newTail].append((0, self._colors[cInt]))
+
+        for a in range(360):
+            angle = self._tails[a]
+            if len(angle) > 0:
+                removals = []
+                for r in range(len(angle)):
+                    tail = angle[r]
+                    if tail[0] <= self.lastRing:
+                        self._drawTail(a, tail[0], tail[1])
+                    if tail[0] - (self._tail - 1) <= self.lastRing:
+                        tail = (tail[0] + amt, tail[1])
+                        self._tails[a][r] = tail
+                    else:
+                        removals.append(tail)
+                for r in removals:
+                    self._tails[a].remove(r)
+
+        self._step = 0
+
+
+class HyperspaceRainbow(BaseCircleAnim):
+    def __init__(self, led, tail=4, growthRate=4, angleDiff=6):
+        super(HyperspaceRainbow, self).__init__(led)
+
+        self._tail = tail
+        self._tails = [[] for x in range(360)]
+        self._growthRate = growthRate
+        self._angleDiff = angleDiff
+
+    def _drawTail(self, angle, ring, color):
+        for i in range(self._tail):
+            if ring - i >= 0 and ring - i <= self.lastRing:
+                level = 255 - ((255 / self._tail) * i)
+                self._led.set(ring - i, angle, colors.color_scale(color, level))
+
+    def step(self, amt=1):
+        self._led.all_off()
+
+        for i in range(self._growthRate):
+            newTail = random.randrange(0, 360, self._angleDiff)
+            self._tails[newTail].append(0)
+
+        for a in range(360):
+            angle = self._tails[a]
+            if len(angle) > 0:
+                removals = []
+                for r in range(len(angle)):
+                    tail = angle[r]
+                    if tail <= self.lastRing:
+                        c = colors.hue2rgb(tail * (255 / self.lastRing))
+                        self._drawTail(a, tail, c)
+                        colors.hue2rgb(tail * (255 / self.lastRing))
+                    if tail - (self._tail - 1) <= self.lastRing:
+                        tail = tail + amt
+                        self._tails[a][r] = tail
+                    else:
+                        removals.append(tail)
+                for r in removals:
+                    self._tails[a].remove(r)
+
+        self._step = 0
+
+
+MANIFEST = [
+    {
+        "class": Hyperspace,
+        "controller": "circle",
+        "desc": None,
+        "display": "Hyperspace",
+        "id": "Hyperspace",
+        "params": [
+            {
+                "default": 4,
+                "help": "Number of new tails per frame",
+                "id": "growthRate",
+                "label": "Growth Rate",
+                "type": "int"
+            },
+            {
+                "default": 4,
+                "help": "Length of tail",
+                "id": "tail",
+                "label": "Length",
+                "type": "int"
+            },
+            {
+                "default": [
+                    [
+                        0,
+                        255,
+                        0
+                    ]
+                ],
+                "help": "Colors of tails",
+                "id": "colors",
+                "label": "Colors",
+                "type": "multi",
+                "controls": {
+                    "default": [
+                        [
+                            0,
+                            255,
+                            0
+                        ]
+                    ],
+                    "help": "Tail color",
+                    "label": "Color",
+                    "type": "color"
+                }
+            }
+        ],
+        "type": "animation"
+    },
+    {
+        "class": HyperspaceRainbow,
+        "controller": "circle",
+        "desc": None,
+        "display": "HyperspaceRainbow",
+        "id": "HyperspaceRainbow",
+        "params": [
+            {
+                "default": 4,
+                "help": "Number of new tails per frame",
+                "id": "growthRate",
+                "label": "Growth Rate",
+                "type": "int"
+            },
+            {
+                "default": 4,
+                "help": "Length of tail",
+                "id": "tail",
+                "label": "Length",
+                "type": "int"
+            }
+        ],
+        "type": "animation"
+    }
+]
--- BiblioPixelAnimations/matrix/MatrixRain.py
+++ BiblioPixelAnimations/matrix/MatrixRain.py
@@ -2,11 +2,14 @@ from bibliopixel.animation import BaseMatrixAnim
 import bibliopixel.colors as colors
 import random
 
+
 class MatrixRain(BaseMatrixAnim):
-    #TODO: Upgrade back to use multiple colors when supported by UI
+    # TODO: Upgrade back to use multiple colors when supported by UI
+
     def __init__(self, led, rain_colors=colors.Green, tail=4, growthRate=4):
         super(MatrixRain, self).__init__(led)
-        if not isinstance(rain_colors, list): rain_colors=[rain_colors]
+        if not isinstance(rain_colors, list):
+            rain_colors = [rain_colors]
         self._colors = rain_colors
         self._tail = tail
         self._drops = [[] for x in range(self._led.width)]
@@ -14,17 +17,17 @@ class MatrixRain(BaseMatrixAnim):
 
     def _drawDrop(self, x, y, color):
         for i in range(self._tail):
-            if y-i >= 0 and y-i < self._led.height:
-                level = 255 - ((255/self._tail)*i)
-                self._led.set(x, y-i, colors.color_scale(color, level))
+            if y - i >= 0 and y - i < self._led.height:
+                level = 255 - ((255 / self._tail) * i)
+                self._led.set(x, y - i, colors.color_scale(color, level))
 
-    def step(self, amt = 1):
+    def step(self, amt=1):
         self._led.all_off()
 
         for i in range(self._growthRate):
             newDrop = random.randint(0, self._led.width - 1)
             cInt = random.randint(0, len(self._colors) - 1)
-            self._drops[newDrop].append((0, self._colors[cInt]));
+            self._drops[newDrop].append((0, self._colors[cInt]))
 
         for x in range(self._led.width):
             col = self._drops[x]
@@ -44,6 +47,7 @@ class MatrixRain(BaseMatrixAnim):
 
         self._step = 0
 
+
 class MatrixRainBow(BaseMatrixAnim):
 
     def __init__(self, led, tail=4, growthRate=4):
@@ -54,16 +58,16 @@ class MatrixRainBow(BaseMatrixAnim):
 
     def _drawDrop(self, x, y, color):
         for i in range(self._tail):
-            if y-i >= 0 and y-i < self._led.height:
-                level = 255 - ((255/self._tail)*i)
-                self._led.set(x, y-i, colors.color_scale(color, level))
+            if y - i >= 0 and y - i < self._led.height:
+                level = 255 - ((255 / self._tail) * i)
+                self._led.set(x, y - i, colors.color_scale(color, level))
 
-    def step(self, amt = 1):
+    def step(self, amt=1):
         self._led.all_off()
 
         for i in range(self._growthRate):
             newDrop = random.randint(0, self._led.width - 1)
-            self._drops[newDrop].append(0);
+            self._drops[newDrop].append(0)
 
         for x in range(self._led.width):
             col = self._drops[x]
@@ -72,7 +76,8 @@ class MatrixRainBow(BaseMatrixAnim):
                 for y in range(len(col)):
                     drop = col[y]
                     if drop < self._led.height:
-                        self._drawDrop(x, drop, colors.hue2rgb(drop * (255/self._led.height)))
+                        self._drawDrop(x, drop, colors.hue2rgb(
+                            drop * (255 / self._led.height)))
                     if drop - (self._tail - 1) < self._led.height:
                         drop = drop + 1
                         self._drops[x][y] = drop
@@ -82,7 +87,6 @@ class MatrixRainBow(BaseMatrixAnim):
                     self._drops[x].remove(r)
 
         self._step = 0
-
 
 
 MANIFEST = [
