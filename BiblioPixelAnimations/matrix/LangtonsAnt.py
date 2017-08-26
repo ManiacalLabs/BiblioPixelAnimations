ecd0d0924f3a0fb349bd4aff301b628147e5f340
Tom Ritchford <tom@swirly.com>
2017-08-26 15:09:41 +0200

    Use inheritance with the LangstonsAnts

--- BiblioPixelAnimations/matrix/LangtonsAnt.py
+++ BiblioPixelAnimations/matrix/LangtonsAnt.py
@@ -3,10 +3,10 @@ from bibliopixel import colors
 import random
 
 
-class LangtonsAnt(BaseMatrixAnim):
+class LangtonsAntBase(BaseMatrixAnim):
 
     def __init__(self, layout, antColor=colors.Green, pathColor=colors.Red):
-        super(LangtonsAnt, self).__init__(layout)
+        super().__init__(layout)
         self.antColor = tuple(antColor)
         self.pathColor = tuple(pathColor)
         self.offColor = colors.Off
@@ -17,7 +17,7 @@ class LangtonsAnt(BaseMatrixAnim):
         self.y = random.randrange(self.height)
         self.d = random.randrange(4)
 
-    def __rollValue(self, val, step, min, max):
+    def _rollValue(self, val, step, min, max):
         val += step
         if val < min:
             diff = min - val
@@ -27,98 +27,68 @@ class LangtonsAnt(BaseMatrixAnim):
             val = min + diff - 1
         return val
 
-    def __changeDir(self, dir):
+    def _changeDir(self, dir):
         if dir:
             dir = 1
         else:
             dir = -1
-        self.d = self.__rollValue(self.d, dir, 0, 3)
+        self.d = self._rollValue(self.d, dir, 0, 3)
 
-    def __moveAnt(self):
+    def _moveAnt(self):
         if self.d == 0:
-            self.y = self.__rollValue(self.y, 1, 0, self.height - 1)
+            self.y = self._rollValue(self.y, 1, 0, self.height - 1)
         elif self.d == 1:
-            self.x = self.__rollValue(self.x, 1, 0, self.width - 1)
+            self.x = self._rollValue(self.x, 1, 0, self.width - 1)
         elif self.d == 2:
-            self.y = self.__rollValue(self.y, -1, 0, self.height - 1)
+            self.y = self._rollValue(self.y, -1, 0, self.height - 1)
         elif self.d == 3:
-            self.x = self.__rollValue(self.x, -1, 0, self.width - 1)
+            self.x = self._rollValue(self.x, -1, 0, self.width - 1)
 
         self.curColor = self.layout.get(self.x, self.y)
+        self._postMove()
+
+
+class LangtonsAnt(LangtonsAntBase):
+    def _postMove(self):
         self.layout.set(self.x, self.y, self.antColor)
 
     def step(self, amt=1):
         if self.curColor == self.pathColor:
             self.layout.set(self.x, self.y, self.offColor)
-            self.__changeDir(False)
-            self.__moveAnt()
+            self._changeDir(False)
+            self._moveAnt()
         else:
             self.layout.set(self.x, self.y, self.pathColor)
-            self.__changeDir(True)
-            self.__moveAnt()
+            self._changeDir(True)
+            self._moveAnt()
 
 
-class LangtonsAntRainbow(BaseMatrixAnim):
+class LangtonsAntRainbow(LangtonsAntBase):
+    DEFAULT_COLORS = [colors.Red, colors.Orange, colors.Yellow,
+                      colors.Green, colors.Blue, colors.Violet]
 
-    def __init__(self, layout, antColor=colors.White):
-        super(LangtonsAntRainbow, self).__init__(layout)
-        self.color_cycle = [colors.Red, colors.Orange,
-                            colors.Yellow, colors.Green,
-                            colors.Blue, colors.Violet]
-        self.antColor = tuple(antColor)
-        self.offColor = colors.Off
-        self.curColor = self.offColor
+    def __init__(self, layout, antColor=colors.White, colors=None):
+        super().__init__(layout, antColor)
+        self.colors = colors or self.DEFAULT_COLORS
         self.curColorIndex = -1
 
-    def pre_run(self):
-        self.x = random.randrange(self.width)
-        self.y = random.randrange(self.height)
-        self.d = random.randrange(4)
-
-    def __rollValue(self, val, step, min, max):
-        val += step
-        if val < min:
-            diff = min - val
-            val = max - diff + 1
-        elif val > max:
-            diff = val - max
-            val = min + diff - 1
-        return val
-
-    def __changeDir(self, dir):
-        if dir:
-            dir = 1
-        else:
-            dir = -1
-        self.d = self.__rollValue(self.d, dir, 0, 3)
-
-    def __moveAnt(self):
-        if self.d == 0:
-            self.y = self.__rollValue(self.y, 1, 0, self.height - 1)
-        elif self.d == 1:
-            self.x = self.__rollValue(self.x, 1, 0, self.width - 1)
-        elif self.d == 2:
-            self.y = self.__rollValue(self.y, -1, 0, self.height - 1)
-        elif self.d == 3:
-            self.x = self.__rollValue(self.x, -1, 0, self.width - 1)
-
-        self.curColor = self.layout.get(self.x, self.y)
+    def _postMove(self):
         if self.curColor == self.offColor:
             i = 0
         else:
-            i = self.color_cycle.index(self.curColor)
+            i = self.colors.index(self.curColor)
             i += 1
-            i %= len(self.color_cycle)
+            i %= len(self.colors)
 
-        self.curColor = self.color_cycle[i]
+        self.curColor = self.colors[i]
         self.curColorIndex = i
         self.layout.set(self.x, self.y, self.antColor)
 
     def step(self, amt=1):
         self.layout.set(self.x, self.y, self.curColor)
         if self.curColorIndex % 2 == 0:
-            self.__changeDir(False)
-            self.__moveAnt()
+            self._changeDir(False)
+            self._moveAnt()
         else:
-            self.__changeDir(True)
-            self.__moveAnt()
+            self._changeDir(True)
+            self._moveAnt()
