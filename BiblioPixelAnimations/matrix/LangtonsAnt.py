8975ae32013f3bff764f29f9e16fe3774dd85717
Tom Ritchford <tom@swirly.com>
2017-08-26 15:45:37 +0200

    Inline two methods on LangstonsAntBase

--- BiblioPixelAnimations/matrix/LangtonsAnt.py
+++ BiblioPixelAnimations/matrix/LangtonsAnt.py
@@ -17,28 +17,26 @@ class LangtonsAntBase(BaseMatrixAnim):
         self.y = random.randrange(self.height)
         self.d = random.randrange(4)
 
-    def _rollValue(self, val, step, min, max):
-        val += step
-        if val < min:
-            diff = min - val
-            val = max - diff + 1
-        elif val > max:
-            diff = val - max
-            val = min + diff - 1
-        return val
+    def _moveAnt(self, delta):
+        def roll(val, step, min, max):
+            val += step
+            if val < min:
+                diff = min - val
+                val = max - diff + 1
+            elif val > max:
+                diff = val - max
+                val = min + diff - 1
+            return val
 
-    def _changeDelta(self, delta):
-        self.d = self._rollValue(self.d, delta, 0, 3)
-
-    def _moveAnt(self):
+        self.d = roll(self.d, delta, 0, 3)
         if self.d == 0:
-            self.y = self._rollValue(self.y, 1, 0, self.height - 1)
+            self.y = roll(self.y, 1, 0, self.height - 1)
         elif self.d == 1:
-            self.x = self._rollValue(self.x, 1, 0, self.width - 1)
+            self.x = roll(self.x, 1, 0, self.width - 1)
         elif self.d == 2:
-            self.y = self._rollValue(self.y, -1, 0, self.height - 1)
+            self.y = roll(self.y, -1, 0, self.height - 1)
         elif self.d == 3:
-            self.x = self._rollValue(self.x, -1, 0, self.width - 1)
+            self.x = roll(self.x, -1, 0, self.width - 1)
 
         self.curColor = self.layout.get(self.x, self.y)
         self._postMove()
@@ -51,12 +49,10 @@ class LangtonsAnt(LangtonsAntBase):
     def step(self, amt=1):
         if self.curColor == self.pathColor:
             self.layout.set(self.x, self.y, self.offColor)
-            self._changeDelta(-1)
-            self._moveAnt()
+            self._moveAnt(-1)
         else:
             self.layout.set(self.x, self.y, self.pathColor)
-            self._changeDelta(1)
-            self._moveAnt()
+            self._moveAnt(1)
 
 
 class LangtonsAntRainbow(LangtonsAntBase):
@@ -83,8 +79,6 @@ class LangtonsAntRainbow(LangtonsAntBase):
     def step(self, amt=1):
         self.layout.set(self.x, self.y, self.curColor)
         if self.curColorIndex % 2 == 0:
-            self._changeDelta(-1)
-            self._moveAnt()
+            self._moveAnt(-1)
         else:
-            self._changeDelta(1)
-            self._moveAnt()
+            self._moveAnt(1)
